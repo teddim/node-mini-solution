a09dcbb894f166b0b713f709db1610eda83c738d
Teddi Maull <teddimaull@gmail.com>
2020-01-03 15:21:40 -0700

    Add solution

---
+++ client/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+// get a quote from the server when the page loads and add it to the dom
+  getQuote();
+
+// when the user enters data and clicks submit, post the quote to the server
+  $('#submit').click((e) => {
+    e.preventDefault();
+    let quote = $('input').val();
+    addQuote(quote);
+  });
+
+  function getQuote(){
+
+    //YOUR CODE HERE, Add a GET request
+    $.get('http://localhost:3000/quote', data => {
+      let quote = $('#quote');
+      quote.append(`<h1>${data}</h1>`);
+    })
+  }
+
+  function addQuote(quote){
+    //YOUR CODE HERE, Add a POST request
+    $.post('http://localhost:3000/quote', quote, response => {
+      $("form")[0].reset(); //clear the input field
+      $('#response').text(`Thank you for your submission: ${response}`);
+    });
+  }
+});
